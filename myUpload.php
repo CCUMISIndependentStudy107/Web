b9c50beedda9108a2899605cc521931cdb8bec3d
leaflu0315 <johnson7511333@gmail.com>
2019-04-23 01:20:34 +0800

    unfinished

--- myUpload.php
+++ myUpload.php
@@ -9,18 +9,20 @@
     /* image upload */
     # 檢查檔案是否上傳成功
     if ($_FILES['product_picture']['error'] === UPLOAD_ERR_OK){
+        $filename = $_FILES['product_picture']['name'];
+        $upload_folder = 'uploads/';
         echo '檔案名稱: ' . $_FILES['product_picture']['name'] . '<br/>';
         echo '檔案類型: ' . $_FILES['product_picture']['type'] . '<br/>';
         echo '檔案大小: ' . ($_FILES['product_picture']['size'] / 1024) . ' KB<br/>';
         echo '暫存名稱: ' . $_FILES['product_picture']['tmp_name'] . '<br/>';
-        if(isImage($_FILES['product_picture']['name'])){
+        if(exif_imagetype($filename)==IMAGETYPE_JPEG || exif_imagetype($filename)==IMAGETYPE_PNG){
             # 檢查檔案是否已經存在
-            if (file_exists('upload/' . $_FILES['product_picture']['name'])){
+            if (file_exists($upload_folder . $_FILES['product_picture']['name'])){
                 echo '檔案已存在。<br/>';
             }
             else {
                 $file = $_FILES['product_picture']['tmp_name'];
-                $dest = 'uploads/' . $_FILES['product_picture']['name'];
+                $dest = $upload_folder . $_FILES['product_picture']['name'];
                 # 將檔案移至指定位置
                 move_uploaded_file($file, $dest);
             }
@@ -28,21 +30,5 @@
     } 
     else{
         echo '錯誤代碼：' . $_FILES['product_picture']['error'] . '<br/>';
-    }
-?>
-
-<?php
-    function isImage($filename){
-        $types = '.gif|.jpg|.png|.bmp| .jpg';  //gif|jepg|png|bmp 四種
-        if(file_exists($filename))
-        {
-            if (($info = getimagesize($filename)) return 0;
-            $ext = image_type_to_extension($info['2']);
-            return stripos($types,$ext);
-        }
-        else
-        {
-            return false;
-        }
     }
 ?>
