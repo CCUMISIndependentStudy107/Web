185aab7b6d9da934e45b0e18c03f5ae6cff9fc4f
leaflu0315 <johnson7511333@gmail.com>
2019-04-23 19:43:46 +0800

    add comment

--- connect_sql.php
+++ connect_sql.php
@@ -1,10 +1,9 @@
 <?php
-$servername = "140.123.94.145";
-$username = "zhbot";
-$password = "zhbot107";
+$servername = "140.123.94.145"; //server 本地可用localhost
+$username = "zhbot";    //sql 帳號
+$password = "zhbot107"; //sql 密碼
 // Create connection
 $conn = mysqli_connect($servername, $username, $password);
-// Check connection
-if (!$conn) die("Connection failed: " . mysqli_connect_error()."<br/>");
-// not die then good
+if (!$conn) die("Connection failed: " . mysqli_connect_error()."<br/>");    //not connected then die
+// not die then good (Y)
 ?> 
