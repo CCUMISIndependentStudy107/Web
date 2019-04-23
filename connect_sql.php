bc1d555fb7d944613eddf88d194272feb7fb178a
leaflu0315 <johnson7511333@gmail.com>
2019-04-23 19:18:06 +0800

    sql connection

--- connect_sql.php
+++ connect_sql.php
@@ -2,11 +2,9 @@
 $servername = "140.123.94.145";
 $username = "zhbot";
 $password = "zhbot107";
-
 // Create connection
 $conn = mysqli_connect($servername, $username, $password);
-
 // Check connection
 if (!$conn) die("Connection failed: " . mysqli_connect_error());
-// echo "Connected successfully";
+// not die then good
 ?> 
