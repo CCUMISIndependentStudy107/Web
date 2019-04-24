35f452922ad82ca5ec4d0119e9ec75f45a8e653f
leaflu0315 <johnson7511333@gmail.com>
2019-04-24 23:45:53 +0800

    change db to utf8

--- connect_sql.php
+++ connect_sql.php
@@ -7,10 +7,10 @@
     $conn = mysqli_connect($servername, $username, $password);  // Create connection
     $db_link = mysql_pconnect($servername, $username, $password);
     mysql_select_db($db_name, $db_link);
-    mysql_query("SET NAMES big5;", $db_link);   //utf8
+    mysql_query("SET NAMES utf8;", $db_link);   //utf8 or big5
     if (!$conn) die("Connection failed: " . mysqli_connect_error()."<br/>");    //not connected then die
     // else echo "Connection Success!";
-    $sql = "CREATE DATABASE IF NOT EXISTS ".$db_name." DEFAULT CHARACTER SET big5 COLLATE big5_chinese_ci;";    //utf8 utf8_unicode_ci
+    $sql = "CREATE DATABASE IF NOT EXISTS ".$db_name." DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;";    //utf8 utf8_unicode_ci or big5 big5_chinese_ci
     if($conn -> query($sql) == false) echo "Failed to create database<br/>";
     // else echo "Database create successfully!<br/>";
 ?> 
