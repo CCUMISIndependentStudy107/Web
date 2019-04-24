3cb779e6437fa9cad18d05290fe9d67ab84f9ffc
leaflu0315 <johnson7511333@gmail.com>
2019-04-24 23:11:34 +0800

    create database default = utf8

--- connect_sql.php
+++ connect_sql.php
@@ -5,9 +5,12 @@
     /* CREATE DATABASE IF NOT EXISTS */
     $db_name = "newzhbot";  //database name
     $conn = mysqli_connect($servername, $username, $password);  // Create connection
+    $db_link = mysql_pconnect($servername, $username, $password);
+    mysql_select_db($db_name, $db_link);
+    mysql_query("SET NAMES utf8;", $db_link);
     if (!$conn) die("Connection failed: " . mysqli_connect_error()."<br/>");    //not connected then die
     // else echo "Connection Success!";
-    $sql = "CREATE DATABASE IF NOT EXISTS ".$db_name;
+    $sql = "CREATE DATABASE IF NOT EXISTS ".$db_name." DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;";
     if($conn -> query($sql) == false) echo "Failed to create database<br/>";
     // else echo "Database create successfully!<br/>";
 ?> 
