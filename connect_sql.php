4a298b12974b9101d9c9d15373a468210e8ec05a
leaflu0315 <johnson7511333@gmail.com>
2019-04-24 22:27:44 +0800

    add something

--- connect_sql.php
+++ connect_sql.php
@@ -2,12 +2,12 @@
     $servername = "140.123.94.145"; //server 本地可用localhost
     $username = "zhbot";    //sql 帳號
     $password = "zhbot107"; //sql 密碼
-    $db_name = "NEWZHBOT";  //database name
-    // Create connection
-    $conn = mysqli_connect($servername, $username, $password);
+    /* CREATE DATABASE IF NOT EXISTS */
+    $db_name = "newzhbot";  //database name
+    $conn = mysqli_connect($servername, $username, $password);  // Create connection
     if (!$conn) die("Connection failed: " . mysqli_connect_error()."<br/>");    //not connected then die
-    // not die then good (Y)
+    // else echo "Connection Success!";
     $sql = "CREATE DATABASE IF NOT EXISTS ".$db_name;
-    if($conn -> query($sql) == false) echo "Failed<br/>";
+    if($conn -> query($sql) == false) echo "Failed to create database<br/>";
     // else echo "Database create successfully!<br/>";
 ?> 
