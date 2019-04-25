3fc1ffe1736d5caaefc83ae34fcd4a575c057d86
leaflu0315 <johnson7511333@gmail.com>
2019-04-26 00:11:43 +0800

    undone

--- Review.php
+++ Review.php
@@ -12,9 +12,35 @@
             $tableName = 'preprocess';
             $sql = "SELECT * FROM ".$tableName." ";
             // echo $sql;
-            $data = mysql_query($sql);
-            for($i=1;$i<=mysql_num_rows($data);$i++) $resarr = mysql_fetch_row($data);
-            for($i=0;$i<count($resarr);$i++) echo $resarr[$i]."<br/>";
+            // $data = mysql_query($sql);
+            // // echo "Count : ".mysql_num_rows($data)."<br/>";
+            // for($i=1;$i<=mysql_num_rows($data);$i++) $resarr = mysql_fetch_row($data);
+            // for($i=0;$i<count($resarr);$i++) echo $resarr[$i]."<br/>";
+            if($result = mysqli_query($conn, $sql)){
+                if(mysqli_num_rows($result) > 0){
+                    echo "<table>";
+                    echo "<tr>";
+                    for($i=0;$i<count($ProductInfoName);$i++)
+                        echo "<th>".$ProductInfoName[$i]."</th>";
+                    echo "</tr>";
+                    while($row = mysqli_fetch_array($result)){
+                        echo "<tr>";
+                        for($i=0;$i<count($ProductInfoName);$i++)
+                            echo "<td>" . $row[$ProductInfoName[$i]] . "</td>";
+                        echo "</tr>";
+                    }
+                    echo "</table>";
+                    // Free result set
+                    mysqli_free_result($result);
+                } else{
+                    echo "No records matching your query were found.";
+                }
+            }
+            else{
+                echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+            }
+            // Close connection
+            mysqli_close($conn);
         ?>
     </body>
 </html>
