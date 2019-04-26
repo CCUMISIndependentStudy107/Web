22b437c084101af1d8c2fc48669e1cc5ebb6b262
leaflu0315 <johnson7511333@gmail.com>
2019-04-26 16:28:41 +0800

    insert value to new table

--- Review.php
+++ Review.php
@@ -26,7 +26,7 @@
 <?php
     include "connect_sql.php";
     $ProductInfoName = array("ID","Name","Price","Quantity","Information","Weight","Tag","PictureName","ReduceC","FolderName","Company");
-    CreateProductTable($servername,$username,$password,$db_name,$ProductInfoName);
+    $newtable = CreateProductTable($servername,$username,$password,$db_name,$ProductInfoName);
     $tableName = 'preprocess';
     $sql = "SELECT * FROM ".$tableName." ";
     // echo $sql;
@@ -35,7 +35,7 @@
     // for($i=1;$i<=mysql_num_rows($data);$i++) $resarr = mysql_fetch_row($data);
     // for($i=0;$i<count($resarr);$i++) echo $resarr[$i]."<br/>";
     $key = "";
-    if(isset($_POST['key'])) $key = $_POST['key'];
+    if(isset($_POST['key'])) $key = $_POST['key'];  //set the key
     if(array_key_exists('button',$_POST)){
         $selected_value = $_POST['Field'];
         if($key !== ""){
@@ -46,6 +46,8 @@
         }
         // echo $sql;
         if($result = mysqli_query($conn, $sql)){
+            $info = array();
+            $id;
             if(mysqli_num_rows($result) > 0){
                 echo "<table>";
                 echo "<tr>";
@@ -56,21 +58,18 @@
                 echo "</tr>";
                 while($row = mysqli_fetch_array($result)){
                     echo "<tr>";
-                    $info = array();
-                    $id;
-                    for($i=0;$i<count($ProductInfoName);$i++){
+                    for($i=0,$j=0;$i<count($ProductInfoName);$i++){
                         echo "<td>" . $row[$ProductInfoName[$i]] . "</td>";
                         $id = $row[$ProductInfoName[0]];
-                        if($i>0) array_push($info,$id."=>".$row[$ProductInfoName[$i]]);
+                        if($i>0) $info[$id][$j++]=$row[$ProductInfoName[$i]];
                     }
-                    // print_r($info);
-                    // echo "<br/>";
                     $confirm = "通過";
                     $deny = "駁回";
                     echo "<td>"."<button name=\"confirm".$id."\">".$confirm."</button>"."<button name=\"deny".$id."\">".$deny."</button>";
                     echo "</tr>";
                 }
                 echo "</table>";
+                // Confirm($ProductInfoName,$newtable,$info,1,$servername,$username,$password,$db_name);
                 // Free result set
                 mysqli_free_result($result);
             }
@@ -95,5 +94,28 @@
         if($conn -> query($sql) == false) echo "Failed to create table ".$tableName."<br/>";
         // else echo "Table create successfully!<br/>";
         return $tableName;
+    }
+
+    function Confirm($ProductInfoName,$table,$array,$id,$servername,$username,$password,$db_name){
+        // print_r($array[$id]);
+        $sql = "INSERT INTO ".$table."(";
+        for($i=1;$i<count($ProductInfoName)-1;$i++) $sql .= $ProductInfoName[$i].",";
+        $sql .= $ProductInfoName[count($ProductInfoName)-1].") VALUES (";
+        for($i=0;$i<count($array[$id]);$i++){
+            switch($i){
+                case 0: case 3: case 5: case 6: case 8:
+                    $sql .= "\"".$array[$id][$i]."\","; //if is string
+                    break;
+                case count($array[$id])-1:
+                    $sql .= "\"".$array[$id][$i]."\");";
+                    break;
+                default:
+                    $sql .= $array[$id][$i].",";
+                    break;
+            }
+        }
+        // echo $sql;
+        $conn = mysqli_connect($servername,$username,$password,$db_name);
+        if($conn -> query($sql) == false) echo "Failed to create table ".$table."<br/>";
     }
 ?>
