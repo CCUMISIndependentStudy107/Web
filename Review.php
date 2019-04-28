b1679baccd63aaf1a8a85a25bcd1f9b4036b7da2
leaflu0315 <johnson7511333@gmail.com>
2019-04-28 20:09:41 +0800

    Add CSS

--- Review.php
+++ Review.php
@@ -3,26 +3,55 @@
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <title>產品審查</title>
         <script src="https://code.jquery.com/jquery-3.4.0.js" integrity="sha256-DYZMCC8HTC+QDr5QNaIcfR7VSPtcISykd+6eSmBW5qo=" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+        <style>
+            .table td, .table th {
+                padding: .75rem;
+                vertical-align: top;
+                border-top: 1px solid #dee2e6;
+                max-width: 160px;
+                white-space: nowrap;
+                text-overflow: ellipsis;
+                overflow: hidden;
+            }
+
+            .badge {
+                margin: 0 2px;
+                cursor: pointer;
+                user-select: none;
+            }
+        </style>
     </head>
     <body>
         <form action="Review.php" method="GET">
-            <p>選擇欄位:
-                <select name="Field">
-                    <option value="Company">廠商名稱</option>
-                    <option value="Name">商品名稱</option>
-                    <option value="Price">產品價格</option>
-                    <option value="Quantity">產品數量</option>
-                    <option value="Information">產品資訊</option>
-                    <option value="Weight">產品重量(g)</option>
-                    <option value="Tag">標籤</option>
-                    <option value="PictureName">產品照面</option>
-                    <option value="FolderName">創建時間</option>
-                    <option value="ReduceC">節碳量</option>
-                    <option value="Checks">審查狀態</option>
-                </select>
-                輸入關鍵字: <input type="text" name="key" value="">
-                <button>送出</button>
-            </p>
+            <div class="form-row">
+                <div class="form-group col-md-4">
+                    <label for="field">選擇欄位</label>
+                    <select class="form-control" name="Field" id="field">
+                        <option value="Company" selected>廠商名稱</option>
+                        <option value="Name">商品名稱</option>
+                        <option value="Price">產品價格</option>
+                        <option value="Quantity">產品數量</option>
+                        <option value="Information">產品資訊</option>
+                        <option value="Weight">產品重量(g)</option>
+                        <option value="Tag">標籤</option>
+                        <option value="PictureName">產品照面</option>
+                        <option value="FolderName">創建時間</option>
+                        <option value="ReduceC">節碳量</option>
+                        <option value="Checks">審查狀態</option>
+                    </select>
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="key">輸入關鍵字</label>
+                    <input type="text" class="form-control" id="key" name="key" value="">
+                </div>
+                <div class="form-group col-md-2">
+                    <label for="">&nbsp;</label>
+                    <button type="submit" class="btn btn-success" style="display: block">送出</button>
+                </div>
+            </div>
         </form>
         <script>
             function ReloadPage(){
@@ -30,57 +59,101 @@
             }
 
             function reviewToCheck(id, status) {
-                $.ajax({
-                    url: 'Review_to_check.php',
-                    type: 'POST',
-                    data: {
-                        id: id,
-                        status: status
-                    },
-                    error: function(xhr) {
-                        alert('Error.');
-                    },
-                    success: function(res) {
-                        // alert('Success.')
-                        console.log(res);
-                        ReloadPage();
-                    }
-                });
+                if (status === 1) {
+                  if (confirm('確定通過？') == true) {
+                      $.ajax({
+                          url: 'Review_to_check.php',
+                          type: 'POST',
+                          data: {
+                              id: id,
+                              status: status
+                          },
+                          error: function(xhr) {
+                              alert('Error.');
+                          },
+                          success: function(res) {
+                              // alert('Success.')
+                              console.log(res);
+                              ReloadPage();
+                          }
+                      });
+                  }
+                }
+                if (status === 2) {
+                  if (confirm('確定駁回？') == true) {
+                      $.ajax({
+                          url: 'Review_to_check.php',
+                          type: 'POST',
+                          data: {
+                              id: id,
+                              status: status
+                          },
+                          error: function(xhr) {
+                              alert('Error.');
+                          },
+                          success: function(res) {
+                              // alert('Success.')
+                              console.log(res);
+                              ReloadPage();
+                          }
+                      });
+                  }
+                }
             }
         </script>
     </body>
 </html>
+
 <?php
     include "connect_sql.php";
 
     if (isset($_GET['Field']) && isset($_GET['key'])) { // Make sure really GET variable(s)
         $ProductInfoName = array("ID", "Name", "Price", "Quantity", "Information", "Weight", "Tag", "PictureName", "ReduceC", "FolderName", "Company", "checks");
+        $ProductInfoName_chinese = array("ID", "商品名稱","商品價格", "上架數量", "商品簡介", "商品重量", "商品標籤", "圖片", "減碳量", "提交日期", "廠商名稱", "狀態");
         $sql = SelectTable($ProductInfoName);
         // To show searching result(s)
         if ($result = mysqli_query($conn, $sql)) {
             if (mysqli_num_rows($result) > 0) {
-                echo "<table>";
+                echo "<div class='table-responsive'>";
+                echo "<table class='table table-striped'>";
+                echo "<thead>";
                 echo "<tr>";
-                for ($i = 0; $i < count($ProductInfoName); $i++)
-                    echo "<th>" . $ProductInfoName[$i] . "</th>";
-                $check = "審查結果";
-                echo "<th>" . $check . "</th>";
+                for ($i = 0; $i < count($ProductInfoName_chinese); $i++)
+                    echo "<th scope='col'>" . $ProductInfoName_chinese[$i] . "</th>";
                 echo "</tr>";
+                echo "</thead>";
+                echo "<tbody>";
                 while ($row = mysqli_fetch_array($result)) {
                     echo "<tr>";
                     for ($i = 0, $j = 0; $i < count($ProductInfoName); $i++) {
-                        echo "<td>" . $row[$ProductInfoName[$i]] . "</td>";
+                        if ($i == count($ProductInfoName) -1) {
+                            switch ($row[$ProductInfoName[$i]]) {
+                                case 1:
+                                    echo "<td><span class='text-success'>PASS</span></td>";
+                                    break;
+                                case 2:
+                                    echo "<td><span class='text-danger'>FAIL</span></td>";
+                                    break;
+                                default:
+                                    echo "<td>";
+                                    echo "<span class='badge badge-success' onclick='reviewToCheck(" . $id . ", 1)'>PASS</span>";
+                                    echo "<span class='badge badge-danger' onclick='reviewToCheck(" . $id . ", 2)'>FAIL</span>";
+                                    echo "</td>";
+                                    break;
+                            }
+                        }
+                        else {
+                            echo "<td title='" . $row[$ProductInfoName[$i]] . "'>" . $row[$ProductInfoName[$i]] . "</td>";
+                        }
                         $id = $row[$ProductInfoName[0]];
                         if ($i > 0)
                             $info[$id][$j++] = $row[$ProductInfoName[$i]];
                     }
-                    echo "<td>";;
-                    echo "<button onclick=\"reviewToCheck(" . $id . ", 1)\">通過</button>";
-                    echo "<button onclick=\"reviewToCheck(" . $id . ", 2)\">駁回</button>";
-                    echo "</td>";
                     echo "</tr>";
                 }
+                echo "</tbody>";
                 echo "</table>";
+                echo "</div>";
                 mysqli_free_result($result);
             }
             else {
@@ -98,8 +171,8 @@
     mysqli_close($conn);
 ?>
 
-<?php 
-    function SelectTable($ProductInfoName){
+<?php
+    function SelectTable($ProductInfoName) {
         $ProductInfoName = array("ID", "Name", "Price", "Quantity", "Information", "Weight", "Tag", "PictureName", "ReduceC", "FolderName", "Company", "checks");
         $selected_value = $_GET['Field'];
         $key = $_GET['key'];
