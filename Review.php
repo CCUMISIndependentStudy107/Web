ce1999dabad7b345628b056d8fd80331aabb1db8
leaflu0315 <johnson7511333@gmail.com>
2019-04-28 12:56:54 +0800

    Modify codes. Add function of checking products.

--- Review.php
+++ Review.php
@@ -5,7 +5,7 @@
         <script src="https://code.jquery.com/jquery-3.4.0.js" integrity="sha256-DYZMCC8HTC+QDr5QNaIcfR7VSPtcISykd+6eSmBW5qo=" crossorigin="anonymous"></script>
     </head>
     <body>
-        <form id="postForm" onsubmit="return false;">
+        <form action="Review.php" method="GET">
             <p>選擇欄位:
                 <select name="Field">
                     <option value="Company">廠商名稱</option>
@@ -19,129 +19,92 @@
                     <option value="FolderName">創建時間</option>
                     <option value="ReduceC">節碳量</option>
                 </select>
-                輸入關鍵字:<input type="text" name="key" value="">
-                <input type="submit" name="button" value="送出"></p>
+                輸入關鍵字: <input type="text" name="key" value="">
+                <button>送出</button>
+            </p>
         </form>
         <script>
-            $(document).ready(function () {
-                $().click(function () {
-                    $.ajax({
-                        url: 'Review.php',
-                        type: 'POST',
-                        data: {
-                            user_name: $('#user_name').val(),
-                        },
-                        error: function(xhr) {
-                            alert('Ajax request 發生錯誤');
-                        },
-                        success: function(response) {
-                            $('#msg_user_name').html(response);
-                            $('#msg_user_name').fadeIn();
-                        }
-                    });
+            function reviewToCheck(id, status) {
+                $.ajax({
+                    url: 'Review_to_check.php',
+                    type: 'POST',
+                    data: {
+                        id: id,
+                        status: status
+                    },
+                    error: function(xhr) {
+                        alert('Error.');
+                    },
+                    success: function(res) {
+                        // alert('Success.')
+                        console.log(res);
+                    }
                 });
-            });
+            }
         </script>
     </body>
 </html>
 <?php
     include "connect_sql.php";
-    $ProductInfoName = array("ID","Name","Price","Quantity","Information","Weight","Tag","PictureName","ReduceC","FolderName","Company");
-    $newtable = CreateProductTable($servername,$username,$password,$db_name,$ProductInfoName);
-    $tableName = 'preprocess';
-    $sql = "SELECT * FROM ".$tableName." ";
-    // echo $sql;
-    // $data = mysql_query($sql);
-    // // echo "Count : ".mysql_num_rows($data)."<br/>";
-    // for($i=1;$i<=mysql_num_rows($data);$i++) $resarr = mysql_fetch_row($data);
-    // for($i=0;$i<count($resarr);$i++) echo $resarr[$i]."<br/>";
-    $key = "";
-    if(isset($_POST['key'])) $key = $_POST['key'];  //set the key
-    if(array_key_exists('button',$_POST)){
-        $selected_value = $_POST['Field'];
-        if($key !== ""){
-            if($selected_value == "Company" || $selected_value == "Name" || $selected_value == "Information" || $selected_value == "Tag" || $selected_value == "PictureName" || $selected_value == "FolderName") 
-                $sql .= "WHERE ".$selected_value." LIKE \"%".$key."%\"";
-            else
-                $sql .= "WHERE ".$selected_value.$key;
-            $sql .= " AND checks = 0";
-        }
-        else $sql .= " WHERE checks = 0";
-        // echo $sql;
-        if($result = mysqli_query($conn, $sql)){
-            $info = array();
-            $id;
-            if(mysqli_num_rows($result) > 0){
+
+    if (isset($_GET['Field']) && isset($_GET['key'])) { // Make sure really GET variable(s)
+        $ProductInfoName = array("ID", "Name", "Price", "Quantity", "Information", "Weight", "Tag", "PictureName", "ReduceC", "FolderName", "Company", "checks");
+
+        $selected_value = $_GET['Field'];
+        $key = $_GET['key'];
+
+        $tableName = 'preprocess';
+        // Complex sql syntax
+        $sql = "SELECT * FROM " . $tableName . " ";
+        if ($selected_value == "Company" ||
+            $selected_value == "Name" ||
+            $selected_value == "Information" ||
+            $selected_value == "Tag" ||
+            $selected_value == "PictureName" ||
+            $selected_value == "FolderName")
+            $sql .= "WHERE " . $selected_value . " LIKE \"%" . $key . "%\"";
+        else
+            $sql .= "WHERE " . $selected_value.$key;
+
+
+        // To show searching result(s)
+        if ($result = mysqli_query($conn, $sql)) {
+            if (mysqli_num_rows($result) > 0) {
                 echo "<table>";
                 echo "<tr>";
-                for($i=0;$i<count($ProductInfoName);$i++)
-                    echo "<th>".$ProductInfoName[$i]."</th>";
+                for ($i = 0; $i < count($ProductInfoName); $i++)
+                    echo "<th>" . $ProductInfoName[$i] . "</th>";
                 $check = "審查結果";
-                echo "<th>".$check."</th>";
+                echo "<th>" . $check . "</th>";
                 echo "</tr>";
-                while($row = mysqli_fetch_array($result)){
+                while ($row = mysqli_fetch_array($result)) {
                     echo "<tr>";
-                    for($i=0,$j=0;$i<count($ProductInfoName);$i++){
+                    for ($i = 0, $j = 0; $i < count($ProductInfoName); $i++) {
                         echo "<td>" . $row[$ProductInfoName[$i]] . "</td>";
                         $id = $row[$ProductInfoName[0]];
-                        if($i>0) $info[$id][$j++]=$row[$ProductInfoName[$i]];
+                        if ($i > 0)
+                            $info[$id][$j++] = $row[$ProductInfoName[$i]];
                     }
-                    $confirm = "通過";
-                    $deny = "駁回";
-                    echo "<td>"."<button name=\"confirm".$id."\">".$confirm."</button>"."<button name=\"deny".$id."\">".$deny."</button>";  //加入事件 Confirm()
+                    echo "<td>";;
+                    echo "<button onclick=\"reviewToCheck(" . $id . ", 1)\">通過</button>";
+                    echo "<button onclick=\"reviewToCheck(" . $id . ", 2)\">駁回</button>";
+                    echo "</td>";
                     echo "</tr>";
                 }
                 echo "</table>";
-                // Confirm($ProductInfoName,$tableName,$newtable,$info,1,$servername,$username,$password,$db_name);  1 改成按鈕
-                // Free result set
                 mysqli_free_result($result);
             }
-            else{
-                echo "No records matching your query were found.";
+            else {
+                // Catch error
+                echo "No records matching your query were found. \n";
             }
         }
-        else{
-            echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+        else {
+            // Catch error
+            echo "ERROR: Could not able to execute $sql " . mysqli_error($conn) . " \n";
         }
     }
+
     // Close connection
     mysqli_close($conn);
-?>
-
-<?php
-    function CreateProductTable($servername,$username,$password,$db_name,$array){
-        $conn = mysqli_connect($servername,$username,$password,$db_name);
-        $tableName = 'product';
-        $sql = "CREATE TABLE IF NOT EXISTS ".$tableName." (".$array[0]." INT NOT NULL AUTO_INCREMENT PRIMARY KEY,".$array[1]." VARCHAR(200),".$array[2]." INT,".$array[3]." INT,".$array[4]." VARCHAR(200),".$array[5]." FLOAT,".$array[6]." VARCHAR(100),".$array[7]." VARCHAR(100),".$array[8]." FLOAT,".$array[9]." VARCHAR(100),".$array[10]." VARCHAR(100));";
-        // echo $sql;
-        if($conn -> query($sql) == false) echo "Failed to create table ".$tableName."<br/>";
-        // else echo "Table create successfully!<br/>";
-        return $tableName;
-    }
-
-    function Confirm($ProductInfoName,$oldtable,$newtable,$array,$id,$servername,$username,$password,$db_name){
-        // print_r($array[$id]);
-        $sql = "INSERT INTO ".$newtable."(";
-        for($i=1;$i<count($ProductInfoName)-1;$i++) $sql .= $ProductInfoName[$i].",";
-        $sql .= $ProductInfoName[count($ProductInfoName)-1].") VALUES (";
-        for($i=0;$i<count($array[$id]);$i++){
-            switch($i){
-                case 0: case 3: case 5: case 6: case 8:
-                    $sql .= "\"".$array[$id][$i]."\","; //if is string
-                    break;
-                case count($array[$id])-1:
-                    $sql .= "\"".$array[$id][$i]."\");";   // the last value
-                    break;
-                default:
-                    $sql .= $array[$id][$i].",";    //number value
-                    break;
-            }
-        }
-        // echo $sql."<br>";
-        $conn = mysqli_connect($servername,$username,$password,$db_name);
-        if($conn -> query($sql) == false) echo "Failed to insert to table ".$newtable."<br/>";
-        $sql = "UPDATE ".$oldtable." SET checks=1 WHERE ".$ProductInfoName[1]."=\"".$array[$id][0]."\" AND ".$ProductInfoName[count($ProductInfoName)-1]."=\"".$array[$id][count($array[$id])-1]."\"";
-        // echo $sql;
-        if($conn -> query($sql) == false) echo "Failed to update table ".$oldtable."<br/>";
-    }
 ?>
---
+++ Review_to_check.php
@@ -0,0 +1,74 @@
+<?php
+    include "connect_sql.php";
+
+    // Create `product` table to database
+    function CreateProductTable($servername, $username, $password, $db_name, $array) {
+        $conn = mysqli_connect($servername, $username, $password, $db_name);
+        $tableName = "product";
+
+        // Create `product` table if not exists
+        $sql = "CREATE TABLE IF NOT EXISTS " . $tableName . " (" . $array[0] . " INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " . $array[1] . " VARCHAR(200), " . $array[2] . " INT, " . $array[3] . " INT, " . $array[4] . " VARCHAR(200), " . $array[5] . " FLOAT, " . $array[6] . " VARCHAR(100), " . $array[7] . " VARCHAR(100), " . $array[8] . " FLOAT, " . $array[9] . " VARCHAR(100), " . $array[10] . " VARCHAR(100));";
+
+        // Catch error: failed to create table
+        if($conn -> query($sql) == false)
+            return "ERROR";
+
+        return $tableName;
+    }
+
+    if (isset($_POST["id"]) && isset($_POST["status"])) { // Make sure really POST variable(s)
+        $id = $_POST["id"];
+        $status = $_POST["status"];
+
+        // "$status == 1" means it is set to be an available product
+        // In addition to update status in database, it IS ABOUT TO be inserted a copy into the new table, `product`
+        if ($status == 1) {
+            $ProductInfoName = array("ID", "Name", "Price", "Quantity", "Information", "Weight", "Tag", "PictureName", "ReduceC", "FolderName", "Company");
+            $newtable = CreateProductTable($servername, $username, $password, $db_name, $ProductInfoName);
+
+            // If got error(s) while creating `product` table, shut down
+            if ($newtable == "ERROR") {
+                echo "Failed to create new table. \n";
+                exit();
+            }
+        }
+
+        // Update status
+        $conn = mysqli_connect($servername, $username, $password, $db_name);
+        $tableName = "preprocess";
+        $sql = "UPDATE `" . $tableName . "` SET `checks`=" . $status . " WHERE `ID`=" . $id . ";";
+
+        // Catch error
+        if ($conn->query($sql) === false)
+            echo "Failed to update STATUS. \n";
+        else
+            echo "Record updated successfully. \n";
+
+        // Check `SUCCESS` and insert a copy into new table
+        if ($status == 1) {
+            $oldtable = "preprocess";
+            // Complex sql syntax
+            $sql = "INSERT INTO " . $newtable . " (";
+            for ($i = 1; $i < count($ProductInfoName) - 1; $i++)
+                $sql .= $ProductInfoName[$i] . ",";
+            $sql .= $ProductInfoName[count($ProductInfoName) - 1] . ") SELECT ";
+            for ($i = 1; $i < count($ProductInfoName) - 1; $i++)
+                $sql .= $ProductInfoName[$i] . ",";
+            $sql .= $ProductInfoName[count($ProductInfoName) - 1];
+            $sql .= " FROM " . $oldtable . " WHERE ID=" . $id . ";";
+
+            // Catch error
+            if($conn -> query($sql) == false)
+                echo "Failed to insert to table " . $newtable . " \n";
+            else
+                echo "Record insert successfully. \n";
+        }
+
+        // Close connection
+        mysqli_close($conn);
+    }
+    else {
+        // Catch error
+        header("Location: Review.php", true, 301);
+        exit();
+    }
