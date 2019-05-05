d35d4662b9912ad64ed705c2c14456972cfdc6dd
leaflu0315 <johnson7511333@gmail.com>
2019-05-06 02:28:21 +0800

    Show company searching results. (( Data, UI

--- Store.php
+++ Store.php
@@ -18,14 +18,24 @@
         .card:hover {
             filter: brightness(.95);
         }
+
+        .card-img-top {
+            width: 100%;
+            height: 350px;
+            object-fit: cover;
+        }
+
+        .form-control-plaintext {
+            font-weight: bold;
+        }
     </style>
 </head>
 <body>
     <!-- <button type="button"><img src=""></button> -->
     <form method="POST" name="Test">
-        <div class="form-row">
+        <div class="form-row mx-0">
             <div class="form-group col-md-6">
-                <label for="CompanyName">廠商名稱</label>
+                <label for="CompanyName">請輸入廠商名稱</label>
                 <input type="text" class="form-control" id="CompanyName" name="CompanyName" required>
             </div>
             <div class="form-group col-md-2">
@@ -34,69 +44,6 @@
             </div>
         </div>
     </form>
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-6 col-md-4">
-                <div class="card text-center mx-1 my-3" data-toggle="modal" data-target="#productDetailModal">
-                    <img class="card-img-top border-bottom" src="uploads/2019-05-04-20-12-06/1.jpg" alt="Success Upload products">
-                    <div class="card-body">
-                        <h5 class="card-title">Name - leaf13</h5>
-                    </div>
-                    <div class="card-footer text-muted">
-                        ReduceC
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-<!-- Modal -->
-<div class="modal fade" id="productDetailModal" tabindex="-1" role="dialog" aria-labelledby="productDetailModalTitle" aria-hidden="true">
-    <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">更多資訊</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <form>
-                    <div class="form-group row">
-                        <label for="name" class="col-sm-1 col-form-label">名稱</label>
-                        <div class="col-sm-3">
-                            <input type="text" readonly class="form-control-plaintext" id="name" value="Name">
-                        </div>
-                        <label for="tag" class="col-sm-1 col-form-label">標籤</label>
-                        <div class="col-sm-7">
-                            <input type="text" readonly class="form-control-plaintext" id="tag" value="tag">
-                        </div>
-                        <label for="price" class="col-sm-1 col-form-label">單價</label>
-                        <div class="col-sm-3">
-                            <input type="text" readonly class="form-control-plaintext" id="price" value="Price">
-                        </div>
-                        <label for="quantity" class="col-sm-1 col-form-label">數量</label>
-                        <div class="col-sm-3">
-                            <input type="text" readonly class="form-control-plaintext" id="quantity" value="quantity">
-                        </div>
-                        <label for="weight" class="col-sm-1 col-form-label">重量</label>
-                        <div class="col-sm-3">
-                            <input type="text" readonly class="form-control-plaintext" id="weight" value="weight">
-                        </div>
-                        <label for="information" class="col-sm-1 col-form-label">資訊</label>
-                        <div class="col-sm-11">
-                            <textarea readonly class="form-control-plaintext" id="information">information</textarea>
-                        </div>
-                    </div>
-                </form>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            </div>
-        </div>
-    </div>
-</div>
-
 
     <?php
         include "connect_sql.php";
@@ -141,11 +88,27 @@
                 $keys = array_keys($arr);
                 $productinfo = array();
                 // print_r($keys);
+
+
+                $result = "<div class=\"container-fluid\"><div class=\"row\">";
+                $modals = "";
+
                 for ($i = 0; $i < count($keys); $i++) {
                     $path = "uploads/";
-                    echo "<div><button type=\"button\"><img src=\"".$path.$arr[$keys[$i]][9]."/".$arr[$keys[$i]][7]."\">".$arr[$keys[$i]][1]."</button></div>";  //print 1=name 9=folder 7=pic
+                    $imgUrl = $path . $arr[$keys[$i]][9] . "/" . $arr[$keys[$i]][7];
+                    $result .= "<div class=\"col-sm-6 col-md-4 col-lg-3\"><div class=\"card text-center mx-1 my-3\" data-toggle=\"modal\" data-target=\"#productDetailModal$i\"><img class=\"card-img-top border-bottom\" src=\"$imgUrl\" alt=\"Success Upload product$i\"><div class=\"card-body\"><h5 class=\"card-title\">".$arr[$keys[$i]][1]."</h5></div><div class=\"card-footer text-muted\">節碳 ".$arr[$keys[$i]][8]." 克</div></div></div>";
+
+                    $modals .= "<div class=\"modal fade\" id=\"productDetailModal$i\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"productDetailModalTitle$i\" aria-hidden=\"true\"><div class=\"modal-dialog modal-xl modal-dialog-centered\" role=\"document\"><div class=\"modal-content\"><div class=\"modal-header\"><h5 class=\"modal-title\">更多資訊</h5><button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button></div><div class=\"modal-body\"><form><div class=\"form-group row\"><label for=\"name$i\" class=\"col-sm-1 col-form-label\">名稱</label><div class=\"col-sm-3\"><input type=\"text\" readonly class=\"form-control-plaintext\" id=\"name$i\" value=\"".$arr[$keys[$i]][1]."\"></div><label for=\"tag$i\" class=\"col-sm-1 col-form-label\">標籤</label><div class=\"col-sm-3\"><input type=\"text\" readonly class=\"form-control-plaintext\" id=\"tag$i\" value=\"".$arr[$keys[$i]][6]."\"></div><label for=\"reduceC$i\" class=\"col-sm-1 col-form-label\">節碳量</label><div class=\"col-sm-3\"><input type=\"text\" readonly class=\"form-control-plaintext\" id=\"reduceC$i\" value=\"".$arr[$keys[$i]][8]." 克\"></div><label for=\"price$i\" class=\"col-sm-1 col-form-label\">單價</label><div class=\"col-sm-3\"><input type=\"text\" readonly class=\"form-control-plaintext\" id=\"price$i\" value=\"".$arr[$keys[$i]][2]." 元\"></div><label for=\"quantity$i\" class=\"col-sm-1 col-form-label\">數量</label><div class=\"col-sm-3\"><input type=\"text\" readonly class=\"form-control-plaintext\" id=\"quantity$i\" value=\"".$arr[$keys[$i]][3]."\"></div><label for=\"weight$i\" class=\"col-sm-1 col-form-label\">重量</label><div class=\"col-sm-3\"><input type=\"text\" readonly class=\"form-control-plaintext\" id=\"weight$i\" value=\"".$arr[$keys[$i]][5]." 克\"></div><label for=\"information$i\" class=\"col-sm-1 col-form-label\">資訊</label><div class=\"col-sm-11\"><textarea readonly class=\"form-control-plaintext\" id=\"information$i\">".$arr[$keys[$i]][4]."</textarea></div></div></form></div><div class=\"modal-footer\"><button type=\"button\" class=\"btn btn-secondary\" data-dismiss=\"modal\">Close</button></div></div></div></div>";
+
+                    // echo "<div><button type=\"button\"><img src=\"".$path.$arr[$keys[$i]][9]."/".$arr[$keys[$i]][7]."\">".$arr[$keys[$i]][1]."</button></div>";  //print 1=name 9=folder 7=pic
                     array_push($productinfo,GetProductInfo($servername,$username,$password,$db_name,$tablename,$arr[$keys[$i]][0]));
                 }
+
+                $result .= "</div></div>";
+
+                echo $result;
+                echo $modals;
+
                 // print_r($productinfo);
                 //[0] => [0~12] see upside [1] => [0~12] ......
                 /*e.g.
