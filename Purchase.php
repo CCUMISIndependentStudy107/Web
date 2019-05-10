921fafc67c214f88f50716b83a27a7ac7de26df9
leaflu0315 <johnson7511333@gmail.com>
2019-05-11 00:51:37 +0800

    remove post test

--- Purchase.php
+++ Purchase.php
@@ -3,28 +3,26 @@
     /* for ($i = 0; $i < count($_POST['AllInfo']); $i++)
         echo $_POST['AllInfo'][$i] . ",";*/
     //[0]=>名稱 [1]=>標籤 [2]=>節碳量 [3]=>單價 [4]=>數量 [5]=>重量 [6]=>資訊 [7]=>購買數量
-    if(isset($_POST['test'])){
-        // $file = "http://140.123.94.145/zhbot/myText.txt";
-        // $file2 = "http://localhost/myText.txt";
-        // $f = fopen($file,"r");
-        // iconv("UTF-8","gbk//TRANSLIT",$f);
-        // $str = "";
-        // while(!feof($f)){
-        //     $str .= fgets($f);
-        // }
-        $ProductChineseName = array("名稱","標籤","節碳量","單價","數量","重量","資訊","購買數量");
-        $product = array();
-        for($i=0;$i<count($_POST['AllInfo']);$i++) array_push($product,$_POST['AllInfo'][$i]);
-        print_r($product);
-        writeInfo($ProductChineseName,$product);
-        /*$url='http://140.123.94.145/zhbot/myText.html';
-        $html= file_get_contents($url);
-        echo $html;
-        // fclose($f);
-        // myPrint($str);
-        myPrint($html);*/
-        // echo $str;
-    }
+    // $file = "http://140.123.94.145/zhbot/myText.txt";
+    // $file2 = "http://localhost/myText.txt";
+    // $f = fopen($file,"r");
+    // iconv("UTF-8","gbk//TRANSLIT",$f);
+    // $str = "";
+    // while(!feof($f)){
+    //     $str .= fgets($f);
+    // }
+    $ProductChineseName = array("名稱","標籤","節碳量","單價","數量","重量","資訊","購買數量");
+    $product = array();
+    for($i=0;$i<count($_POST['AllInfo']);$i++) array_push($product,$_POST['AllInfo'][$i]);
+    print_r($product);
+    writeInfo($ProductChineseName,$product);
+    /*$url='http://140.123.94.145/zhbot/myText.html';
+    $html= file_get_contents($url);
+    echo $html;
+    // fclose($f);
+    // myPrint($str);
+    myPrint($html);*/
+    // echo $str;
 ?>
 <?php
     header("Content-Type:text/html; charset=big5");
