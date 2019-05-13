9464befc3a5f8f4e0d41c6c0140d2e98ad8661ce
honginho <sm870831@gmail.com>
2019-05-13 13:08:52 +0800

    Purchase record section done.

--- Self.php
+++ Self.php
@@ -3,130 +3,32 @@
     include "SQLRelative.php";
     if (isset($_POST['username'])){
         $name = $_POST['username'];
-        // $name = 'zhbot';
-        $eth = getEthernet($servername,$username,$password,$db_name,$name);
+        $eth = getEthernet($servername, $username, $password, $db_name, $name);
         echo $eth;
-    }
-?>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>訂單</title>
-        <script src="https://code.jquery.com/jquery-3.4.0.js" integrity="sha256-DYZMCC8HTC+QDr5QNaIcfR7VSPtcISykd+6eSmBW5qo=" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-        <script src="./web3.min.js"></script>
-        <script src="./blockchain.js"></script>
-        <style>
-            .table td, .table th {
-                padding: .75rem;
-                vertical-align: top;
-                border-top: 1px solid #dee2e6;
-                max-width: 160px;
-                white-space: nowrap;
-                text-overflow: ellipsis;
-                overflow: hidden;
-            }
 
-            .badge {
-                margin: 0 2px;
-                cursor: pointer;
-                user-select: none;
-            }
-        </style>
-    </head>
-    <body>
-        <form action="Order.php" method="GET">
-            <div class="form-row mx-0">
-                <div class="form-group col-md-6">
-                    <label for="key">廠商名稱</label>
-                    <input type="text" class="form-control" id="key" name="key" value="">
-                </div>
-                <div class="form-group col-md-2">
-                    <label for="">&nbsp;</label>
-                    <button type="submit" class="btn btn-success" style="display: block">送出</button>
-                </div>
-            </div>
-        </form>
-        <script>
-            function ReloadPage(){
-                window.location.reload();
-            }
+        // To split `$eth` and `#record-table` as <hr>
+        // 94 分隔線分隔線分隔線分隔線分隔線的部分
+        echo "8877887";
 
-            function orderToCheck(id, status, reduceC = 0) {
-                if (status === 1) {
-                    if (confirm('確定通過？') == true) {
-                        let toAddr = $('#record' + id).text();
-                        sendHDC(reduceC, toAddr).then(tx => {
-                            $.ajax({
-                                url: 'Order_to_check.php',
-                                type: 'POST',
-                                data: {
-                                    id: id,
-                                    status: status,
-                                    tx: tx
-                                },
-                                error: function(xhr) {
-                                    alert('Error.');
-                                },
-                                success: function(res) {
-                                    // alert('Success.')
-                                    console.log(res);
-                                    ReloadPage();
-                                }
-                            });
-                        }).catch(err => {
-                            console.log(err);
-                        });;
-                    }
-                }
-                if (status === 2) {
-                    if (confirm('確定駁回？') == true) {
-                        $.ajax({
-                            url: 'Order_to_check.php',
-                            type: 'POST',
-                            data: {
-                                id: id,
-                                status: status
-                            },
-                            error: function(xhr) {
-                                alert('Error.');
-                            },
-                            success: function(res) {
-                                // alert('Success.')
-                                console.log(res);
-                                ReloadPage();
-                            }
-                        });
-                    }
-                }
-            }
-        </script>
-    </body>
-</html>
-
-<?php
-    include "connect_sql.php";
-    include "SQLRelative.php";
-    if (isset($_GET['key'])) { // Make sure really GET variable(s)
         $tablename = "record";
         $fieldname = GetFieldName($servername, $username, $password, $db_name, $tablename);
-        $sql = SelectTable($tablename,$fieldname,$key);
+        $cardID = getCIDBYName($servername, $username, $password, $db_name, $name);
+        $sql = SelectTable($tablename, $fieldname, $cardID);
         // print_r($fieldname); die();
         // [0] => ID [1] => CardID [2] => ProductID [3] => Price [4] => Quantity [5] => Time [6] => Company [7] => tx [8] => Status
-        // echo $sql;
+        // echo $sql; die();
         // To show searching result(s)
         $OrderInfo = array();
         $fieldname_chinese = array("ID", "買方","商品名稱", "商品價格", "購買數量", "提交日期", "廠商名稱", "狀態");
         if ($result = mysqli_query($conn, $sql)) {
             if (mysqli_num_rows($result) > 0) {
+                echo "<h3>購買紀錄</h3>";
                 echo "<div class='table-responsive'>";
                 echo "<table class='table table-striped'>";
                 echo "<thead>";
                 echo "<tr>";
                 for ($i = 0; $i < count($fieldname_chinese); $i++) {
-                    if ($i != 0) {
+                    if ($i != 0 && $i != 1) {
                         echo "<th scope='col'>" . $fieldname_chinese[$i] . "</th>";
                     }
                 }
@@ -153,10 +55,7 @@
                                     echo "<td><span class='text-danger'>FAIL</span></td>";
                                     break;
                                 default:
-                                    echo "<td>";
-                                    echo "<span class='badge badge-success' onclick='orderToCheck(" . $id . ", 1, " . $reduceC . ")'>PASS</span>";
-                                    echo "<span class='badge badge-danger' onclick='orderToCheck(" . $id . ", 2)'>FAIL</span>";
-                                    echo "</td>";
+                                    echo "<td><span class='text-warning'>PENDING(" . $txLink . ")</span></td>";
                                     break;
                             }
                         }
@@ -167,13 +66,8 @@
                                 $eth = getEthernetBYCID($servername, $username, $password, $db_name, $id);
                                 echo "<td id='record" . $id . "' style='display: none'>" . $eth . "</td>";
                             }
-                            // 不要顯示 `tx(7)`
-                            else if ($i == 7) {}
-                            else if ($i == 1) {
-                                $id = $row[$fieldname[0]];
-                                $name = getNameBYCID($servername, $username, $password, $db_name, $id);
-                                echo "<td title='" . $name . "'>" . $name . "</td>";
-                            }
+                            else if ($i == 7) { /* 不要顯示 `tx(7)` */ }
+                            else if ($i == 1) { /* 不必要顯示買方名稱 */ }
                             else if ($i == 2) {
                                 $id = $row[$fieldname[0]];
                                 $name = getProductBYID($servername, $username, $password, $db_name, $id);
@@ -208,15 +102,13 @@
             // Catch error
             echo "ERROR: Could not able to execute $sql " . mysqli_error($conn) . " \n";
         }
+
+        // Close connection
+        mysqli_close($conn);
     }
 
-    // Close connection
-    mysqli_close($conn);
-?>
-
-<?php
     function SelectTable($tablename,$fieldname,$key) {
-        $sql = "SELECT * FROM $tablename WHERE $fieldname =\"$key\" ORDER BY id DESC";
+        $sql = "SELECT * FROM $tablename WHERE $fieldname[1] =\"$key\" ORDER BY id DESC";
         return $sql;
     }
 
@@ -249,23 +141,19 @@
         return $name;
     }
 
-    function getNameBYCID($servername,$username,$password,$db_name,$id){
+    function getCIDBYName($servername,$username,$password,$db_name,$name){
         $member_table = "member";
         $member_fieldname = GetFieldName($servername, $username, $password, $db_name, $member_table);
-        $record_table = "record";
-        $record_fieldname = GetFieldName($servername, $username, $password, $db_name, $record_table);
         // print_r($member_fieldname);
-        //[0] => ID [1] => name [2] => CardID [3] => Ether [4] => HDC
-        // print_r($record_fieldname);
-        //[0] => ID [1] => CardID [2] => ProductID [3] => Price [4] => Quantity [5] => Time [6] => Company [7] => Status
-        $sql = "SELECT ".$member_table.".".$member_fieldname[1]." FROM ".$member_table.",".$record_table." WHERE ".$member_table.".".$member_fieldname[2]."=".$record_table.".".$record_fieldname[1]." AND ".$record_table.".".$member_fieldname[0]."=".$id.";";
+        // [0] => ID [1] => name [2] => CardID [3] => Ether [4] => HDC
+        $sql = "SELECT ".$member_fieldname[2]." FROM ".$member_table." WHERE ".$member_fieldname[1]."=\"".$name."\";";
         // echo $sql; die();
         $conn = mysqli_connect($servername, $username, $password, $db_name);
-        $name;
+        $cid;
         if ($res = mysqli_query($conn, $sql)) {
             if (mysqli_num_rows($res) > 0) {
                 while ($row = mysqli_fetch_array($res)) {
-                    $name = $row[$member_fieldname[1]];
+                    $cid = $row[$member_fieldname[2]];
                 }
             }
             else {
@@ -274,8 +162,8 @@
             }
             mysqli_free_result($res);
         }
-        // print($name);
-        return $name;
+        // print($cid);
+        return $cid;
     }
 
     function getEthernetBYCID($servername,$username,$password,$db_name,$id){
@@ -353,8 +241,7 @@
         $reduce = (float)$quantity*$weight*0.44;
         return $reduce;
     }
-?>
-<?php
+
     function getEthernet($servername,$username,$password,$db_name,$name){
         $member_table = "member";
         $member_fieldname = GetFieldName($servername, $username, $password, $db_name, $member_table);
