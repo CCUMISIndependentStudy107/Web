614dcf0d62f205db658e955ed816dae90b3b2495
leaflu0315 <johnson7511333@gmail.com>
2019-05-13 22:06:44 +0800

    get information from proile

---
+++ Alpha_table_get.php
@@ -0,0 +1,36 @@
+<?php
+    include "connect_sql.php";
+    include "SQLRelative.php";
+    $tablename = "profile";
+    $companyname = "leaflu";
+    $AllInfo = getAll($servername,$username,$password,$db_name,$tablename,$companyname);
+    // print_r($AllInfo);
+    // [0] => Company [1] => ProductName [2] => Quantity [3] => Material [4] => Weight [5] => Contract [6] => Image [7] => Date
+    $tx = $AllInfo[5];
+    // print($tx);
+?>
+
+<?php
+    function getAll($servername,$username,$password,$db_name,$tablename,$companyname){
+        $conn = mysqli_connect($servername,$username,$password,$db_name);
+        $fieldname = GetFieldName($servername,$username,$password,$db_name,$tablename);
+        // print_r($fieldname);
+        // [0] => ID [1] => Company [2] => ProductName [3] => Quantity [4] => Material [5] => Weight [6] => Contract [7] => Image [8] => Date
+        $sql = "SELECT * FROM ".$tablename." WHERE $fieldname[1] = \"$companyname\"";
+        // echo $sql;
+        $arr = array();
+        if($res = mysqli_query($conn, $sql)){
+            while($row = mysqli_fetch_array($res)){
+                for($i=0,$j=0;$i<count($fieldname);$i++){
+                    // echo "<td>" . $row[$ProductInfoName[$i]] . "</td>";
+                    $id = $row[$fieldname[0]];
+                    if($i>0) $arr[$id][$j++]=$row[$fieldname[$i]];
+                }
+            }
+        }
+        // print_r($arr);
+        $keys = array_keys($arr);
+        // print_r($keys);
+        return $arr[$keys[0]];
+    }
+?>
